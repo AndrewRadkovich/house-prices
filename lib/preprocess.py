09e29a4591feb83882fe4b30ae5c515a8b23e61b
Andrei Radkovich <a.radkovich@godel.first-utility.com>
2018-11-20 19:07:19 +0300

    pca to 2 components, plot 3d

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -1,7 +1,10 @@
-from json import dumps
-from logging import warning
+from logging import warning, info
 
+import numpy
 import pandas
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 
@@ -21,6 +24,7 @@ def load_data():
 def extract_meta_info(csv):
     meta = {}
     keys = csv.keys()
+    info("Shape: {}".format(csv.shape))
     for key in keys:
         meta[key] = {}
         value_set = set(csv[key])
@@ -59,7 +63,27 @@ def data_norm(data, meta_info):
 if __name__ == '__main__':
     train_x, train_y, test_x = load_data()
     meta_info_train_x = extract_meta_info(train_x)
-    print(dumps(meta_info_train_x, indent=2))
-    normalized_data = data_norm(train_x, meta_info_train_x)
-    print(dumps(normalized_data, indent=2))
+    meta_info_test_x = extract_meta_info(test_x)
+    # print(dumps(meta_info_train_x, indent=2))
+    # print(dumps(meta_info_test_x, indent=2))
 
+    normalized_train_x = data_norm(train_x, meta_info_train_x)
+    normalized_test_x = data_norm(test_x, meta_info_test_x)
+
+    pca = PCA(n_components=2)
+    pca.fit(train_x)
+    print(pca.explained_variance_ratio_)
+    print(pca.singular_values_)
+
+    print(pca.transform([train_x.values[0]]), train_y[0])
+    print(pca.transform([train_x.values[100]]), train_y[100])
+    print(pca.transform([train_x.values[50]]), train_y[50])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    pca_transform = pca.transform(train_x.values)
+    Y = pca_transform[:, 1]
+    X = pca_transform[:, 0]
+    Z = numpy.array([numpy.array([x]) for x in train_y.values])
+    ax.plot_wireframe(X, Y, Z)
+    plt.show()
