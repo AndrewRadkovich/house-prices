125edc9eed52632bb6f6d6e8df6c4b321b90ae19
Andrei Radkovich <a.radkovich@godel.first-utility.com>
2018-11-22 18:03:56 +0300

    generate correlation plots

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -1,10 +1,9 @@
+from json import dumps
 from logging import warning, info
 
+import matplotlib.pyplot as plt
 import numpy
 import pandas
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 
@@ -36,9 +35,11 @@ def extract_meta_info(csv):
 
         if "<class 'str'>" in meta[key]["type"]:
             meta[key]["value_set"] = list(value_set)
-        elif meta[key]["type"] in ["<class 'int'>", "<class 'float'>"]:
+        else:
             meta[key]["stats"] = {
-                "mean": csv[key].mean()
+                "mean": csv[key].mean(),
+                "max": float(csv[key].max()),
+                "min": float(csv[key].min())
             }
     return meta
 
@@ -64,37 +65,33 @@ def to_2d_array(array):
     return numpy.array([numpy.array([x]) for x in array])
 
 
+def generate_correlation_plots():
+    fig, ax = plt.subplots(20, 4, figsize=(20, 80))
+    for feature_idx, feature in enumerate(train_x.keys()):
+        values = train_x[feature].values
+        x_axis = int(feature_idx / 4)
+        y_axis = feature_idx % 4
+
+        if len(meta_info_train_x[feature]["type"]) > 1:
+            le = LabelEncoder()
+            string_values = list(map(lambda v: str(v), values))
+            values = le.fit_transform(string_values)
+
+        ax[x_axis][y_axis].plot(values, train_y, 'x')
+        ax[x_axis][y_axis].set_xlabel(feature)
+        ax[x_axis][y_axis].set_ylabel('SalePrice')
+    fig.savefig("test.png")
+
+
 if __name__ == '__main__':
     train_x, train_y, test_x = load_data()
     meta_info_train_x = extract_meta_info(train_x)
     meta_info_test_x = extract_meta_info(test_x)
-    # print(dumps(meta_info_train_x, indent=2))
-    # print(dumps(meta_info_test_x, indent=2))
 
-    normalized_train_x = data_norm(train_x, meta_info_train_x)
-    normalized_test_x = data_norm(test_x, meta_info_test_x)
+    for label in meta_info_train_x:
+        print(label + " =============================================")
+        print(dumps(meta_info_train_x[label], indent=2))
+        print(dumps(meta_info_test_x[label], indent=2))
 
-    fig, ax = plt.subplots()
-
-    ax = fig.add_subplot(111, projection='3d')
-    X = to_2d_array(PCA(n_components=1).fit_transform(train_x[["YearBuilt"]].values))
-
-    Y = to_2d_array(PCA(n_components=1).fit_transform(train_x[["GarageType",
-                                                               "GarageYrBlt",
-                                                               "GarageFinish",
-                                                               "GarageCars",
-                                                               "GarageArea",
-                                                               "GarageQual",
-                                                               "GarageCond"]].values))
-    Z = to_2d_array(train_y.values)
-    ax.scatter(X, Y, Z)
-
-    ax.set_xlabel('YearBuilt')
-    ax.set_ylabel('Functional')
-    ax.set_zlabel('SalePrice')
-
-    # ax.plot(to_2d_array(train_x["YearBuilt"].values), to_2d_array(train_y.values), 'x')
-    # ax.plot(pca_transform, to_2d_array(train_y.values), 'x')
-    ax.grid()
-    # fig.savefig("test.png")
-    plt.show()
+    # normalized_train_x = data_norm(train_x, meta_info_train_x)
+    # normalized_test_x = data_norm(test_x, meta_info_test_x)
