54883b6939ca3c57a55ec8a8a12f7f8cd4814584
Andrei Radkovich <a.radkovich@godel.first-utility.com>
2018-12-28 15:50:54 +0300

    refactoring

--- houseprices/pipeline.py
+++ houseprices/pipeline.py
@@ -3,6 +3,7 @@ import logging as log
 import numpy as np
 import pandas as pd
 from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
@@ -105,12 +106,15 @@ def root_mean_square_error(y_predicted, y_actual):
     return np.sqrt(metrics.mean_squared_error(y_actual, y_predicted))
 
 
-def predict(scaled_submission):
-    return lambda r: r["model"].predict(scaled_submission)
-
-
 def assert_has_no_nan(array: np.array):
     assert not np.isnan(array).any()
+
+
+def overall_qual_filter(overall_qual_value):
+    def _overall_qual_filter(df):
+        return df["OverallQual"] == overall_qual_value
+
+    return _overall_qual_filter
 
 
 def run_pipeline(remove_outliers_op, cv=KFold(n_splits=5), estimator_factory=None):
@@ -123,55 +127,48 @@ def run_pipeline(remove_outliers_op, cv=KFold(n_splits=5), estimator_factory=Non
 
     train = remove_outliers_op(train)
 
-    train = train[train["OverallQual"] < 5]
-    submission = submission[submission["OverallQual"] < 5]
+    # train = train[overall_qual_filter(5)(train)]
+    # submission = submission[overall_qual_filter(5)(submission)]
 
     target = train["SalePrice"]
     data = train.drop(["SalePrice", "Id", "GarageArea"], axis=1)
-    submission = submission.drop(["Id", "GarageArea"], axis=1)
-
-    log.info("train data shape: {}".format(data.shape))
-    log.info("submission shape: {}".format(submission.shape))
+    submission = submission.drop(["GarageArea"], axis=1)
+    submission_ids = submission["Id"]
+    submission_rows = submission.drop(["Id"], axis=1)
+    submission_by_id = {
+        "id": submission_ids,
+        "rows": submission_rows
+    }
 
     # fill nans
     log.info("data preprocessing...")
-    preprocessed_data, preprocessed_submission = preprocess_data(data), preprocess_data(submission)
-    log.info("train data shape: {}".format(preprocessed_data.shape))
-    log.info("submission shape: {}".format(preprocessed_submission.shape))
+    preprocessed_data = preprocess_data(data)
 
     # encode labels
     log.info("encode labels...")
     labels_by_column = read_json("../dataset/meta/labels.json")
-    encoded_submission = encode_labels(preprocessed_submission, labels_by_column)
     encoded_data = encode_labels(preprocessed_data, labels_by_column)
-    log.info("train data shape: {}".format(encoded_data.shape))
-    log.info("submission shape: {}".format(encoded_submission.shape))
 
     # scale data
     log.info("scale data...")
     min_max_scaler = MinMaxScaler()
     min_max_scaler.fit(encoded_data.values)
     scaled_data = min_max_scaler.transform(encoded_data.values)
-    scaled_submission = min_max_scaler.transform(encoded_submission.values)
-
     assert_has_no_nan(scaled_data)
-    assert_has_no_nan(scaled_submission)
 
     sale_price_converter = SalePriceConverter()
     scaled_target = sale_price_converter.scale(target.values.reshape(-1, 1)).reshape(1, -1)[0]
 
     # run models
     log.info("run models...")
-    results = []
 
     description = str(estimator_factory())
     log.info("model: " + description)
-    result = {
-        "description": description,
-        "model_factory": estimator_factory,
-        "launches": []
-    }
     try:
+        result = {
+            "description": description,
+            "launches": []
+        }
         for train_index, test_index in cv.split(scaled_data, scaled_target):
             train_data, test_data = scaled_data[train_index], scaled_data[test_index]
             train_target, test_target = scaled_target[train_index], scaled_target[test_index]
@@ -186,69 +183,76 @@ def run_pipeline(remove_outliers_op, cv=KFold(n_splits=5), estimator_factory=Non
 
             result["launches"].append({
                 "rmse": rmse,
-                "model": estimator,
-                "data_indexes": {
-                    "train_index": train_index,
-                    "test_index": test_index
-                }
+                "model": estimator
             })
         rmse_list = np.array(list(map(lambda r: r["rmse"], result["launches"])))
         log.info("mean rmse: {}±{}".format(rmse_list.mean(), rmse_list.std()))
-        results.append(result)
 
-        make_fold_average_prediction(results, sale_price_converter, scaled_submission)
-        make_fullset_prediction(estimator_factory, sale_price_converter, scaled_data, scaled_submission, scaled_target)
+        estimators = list(map(lambda launch: launch["model"], result["launches"]))
+        kfold_mean_submissions = make_submission(estimators, min_max_scaler, sale_price_converter, submission_by_id)
+        save_submissions("{}±{}-kfold_mean.csv".format(rmse_list.mean(), rmse_list.std()), kfold_mean_submissions)
+
+        fullset_estimator = estimator_factory()
+        fullset_estimator.fit(X=scaled_data, y=scaled_target)
+        fullset_rmse = root_mean_square_error(fullset_estimator.predict(X=scaled_data), scaled_target)
+        log.info("full set rmse: {}".format(fullset_rmse))
+        fullset_submissions = make_submission([fullset_estimator], min_max_scaler, sale_price_converter,
+                                              submission_by_id)
+        save_submissions("{}-fullset.csv".format(fullset_rmse), fullset_submissions)
     except Exception as e:
         error_message = "Error occurred while executing {}: {}".format(description, e)
         log.error(error_message)
-        result["error"] = error_message
 
 
-def make_fullset_prediction(estimator_factory, sale_price_converter, scaled_data, scaled_submission, scaled_target):
-    estimator = estimator_factory()
-    estimator.fit(scaled_data, scaled_target)
-    fullset_predicted = estimator.predict(scaled_data)
-    fullset_rmse = root_mean_square_error(y_predicted=fullset_predicted, y_actual=scaled_target)
-    log.info("fullset rmse: {}".format(fullset_rmse))
-    log.info("fullset score: {}".format(estimator.score(scaled_data, scaled_target)))
-    full_set_submission = estimator.predict(scaled_submission)
-    # save_submissions(sale_price_converter.inv_scale(full_set_submission), "-fullset")
+def make_submission(estimators, train_data_scaler, sale_price_converter, submissions_by_id) -> pd.DataFrame:
+    labels_by_column = read_json("../dataset/meta/labels.json")
+
+    def encode(data):
+        return encode_labels(data, labels_by_column).values
+
+    def scale(data):
+        return train_data_scaler.transform(data)
+
+    def predict_single(single_submission):
+        return lambda r: r.predict(single_submission)
+
+    def predict(x_value_single):
+        return list(map(predict_single(x_value_single), estimators))
+
+    def mean(values):
+        return np.mean(np.array(values), axis=0)
+
+    def inv_scale(target):
+        return sale_price_converter.inv_scale(target).reshape(1, -1)[0]
+
+    rows = submissions_by_id["rows"]
+    scaled_rows = scale(encode(preprocess_data(rows)))
+    predictions = predict(scaled_rows)
+    predictions_mean = mean(predictions)
+    sale_price = inv_scale(predictions_mean)
+    return pd.DataFrame({"Id": submissions_by_id["id"], "SalePrice": sale_price})
 
 
-def make_fold_average_prediction(results, sale_price_converter, scaled_submission):
-    for result in results:
-        launches = result["launches"]
-        scaled_submission_predicted_mean = np.mean(np.array(list(map(predict(scaled_submission), launches))),
-                                                   axis=0)
-        kfold_average_submissions = sale_price_converter.inv_scale(scaled_submission_predicted_mean)
-        # save_submissions(kfold_average_submissions, "-average")
+def grlivarea_top2_bottom_right(train):
+    return train.drop(train[(train["GrLivArea"] > 4500) & (train["SalePrice"] < 300000)].index)
 
 
-def remove_grlivarea_top2_right(train):
-    train = train.drop(train[(train["GrLivArea"] > 4500) & (train["SalePrice"] < 300000)].index)
-    # train = train.drop(train[train["TotalBsmtSF"] > 3000].index)
-    # train = train.drop(train[train["SalePrice"] > 700000].index)
-    return train
-
-
-def save_submissions(submissions, filename_postfix):
-    output_df = pd.DataFrame(data={"Id": range(1461, 2920), "SalePrice": submissions.reshape(1, -1)[0]})
-    filename = "submission{}.csv".format(filename_postfix)
-    output_df.to_csv(filename, index=False)
+def save_submissions(filename, submissions: pd.DataFrame):
+    submissions.to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
-    # run_pipeline(remove_outliers_op=remove_grlivarea_top2_right,
-    #              cv=KFold(n_splits=5),
-    #              estimator_factory=lambda: RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
-    #                                                              max_features='auto', max_leaf_nodes=None,
-    #                                                              min_impurity_decrease=0.0, min_impurity_split=None,
-    #                                                              min_samples_leaf=1, min_samples_split=2,
-    #                                                              min_weight_fraction_leaf=0.0, n_estimators=500,
-    #                                                              n_jobs=16,
-    #                                                              oob_score=False, random_state=None, verbose=0,
-    #                                                              warm_start=False))
+    run_pipeline(remove_outliers_op=grlivarea_top2_bottom_right,
+                 cv=KFold(n_splits=5),
+                 estimator_factory=lambda: RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=None,
+                                                                 max_features='auto', max_leaf_nodes=None,
+                                                                 min_impurity_decrease=0.0, min_impurity_split=None,
+                                                                 min_samples_leaf=1, min_samples_split=2,
+                                                                 min_weight_fraction_leaf=0.0, n_estimators=50,
+                                                                 n_jobs=16,
+                                                                 oob_score=False, random_state=None, verbose=0,
+                                                                 warm_start=False))
 
-    run_pipeline(remove_outliers_op=remove_grlivarea_top2_right,
+    run_pipeline(remove_outliers_op=grlivarea_top2_bottom_right,
                  cv=KFold(n_splits=5),
                  estimator_factory=lambda: LinearRegression(fit_intercept=False))
